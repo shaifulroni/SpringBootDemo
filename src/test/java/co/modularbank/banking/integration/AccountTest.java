7bd3ded14fd4b26c17fdfa6066bdeaa0fcc78faf
Shaiful <shaiful.cse@gmail.com>
2021-07-22 02:35:28 +0600

    Added Account and Transaction Integration test.

---
+++ src/test/java/co/modularbank/banking/integration/AccountTest.java
@@ -0,0 +1,146 @@
+package co.modularbank.banking.integration;
+
+import co.modularbank.banking.amqp.RabbitMessageListener;
+import co.modularbank.banking.controller.model.AccountRequest;
+import co.modularbank.banking.domain.Customer;
+import co.modularbank.banking.mapper.CustomerMapper;
+import co.modularbank.banking.service.AccountService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    CustomerMapper customerMapper;
+
+    @Autowired
+    AccountService accountService;
+
+    @MockBean
+    RabbitMessageListener rabbitMessageListener;
+
+    private long customerId;
+
+    @BeforeEach
+    void setup() {
+        Customer newCustomer = new Customer("FirstName", "LastName", UUID.randomUUID() + "@email.com", "8801231234");
+        customerId = customerMapper.insertCustomer(newCustomer);
+    }
+
+    @Test
+    void givenAccountId_whenGetAccountById_thenVerifyAccountExists() throws Exception {
+        AccountRequest accountRequest = new AccountRequest();
+        accountRequest.setCustomerId(customerId);
+        accountRequest.setCountryCode("BD");
+        Set<String> currencyList = new HashSet<>();
+        currencyList.add("SEK");
+        currencyList.add("GBP");
+        accountRequest.setCurrencyCodes(currencyList);
+
+        long accountId = accountService.createAccount(accountRequest).getAccountId();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/account/" + accountId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountId").value(accountId))
+                .andExpect(jsonPath("$.customerId").value(customerId))
+                .andExpect(jsonPath("$.balance[0].amount").value(0.0))
+                .andExpect(jsonPath("$.balance[0].currency").isString())
+                .andExpect(jsonPath("$.balance[1].amount").value(0.0))
+                .andExpect(jsonPath("$.balance[1].currency").isString());
+    }
+
+    @Test
+    void givenInvalidAccountId_whenGetAccountById_thenVerifyAccountExists() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/account/99999")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("No account found"));
+    }
+
+    @Test
+    void givenCustomerCountryBalance_whenInsertAccount_thenVerifyAccountCreated() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"customerId\":" + customerId +",\"countryCode\":\"BD\",\"currencyCodes\":[\"USD\",\"EUR\"]}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountId").isNumber())
+                .andExpect(jsonPath("$.customerId").value(customerId))
+                .andExpect(jsonPath("$.balance[0].amount").value(0.0))
+                .andExpect(jsonPath("$.balance[0].currency").isString())
+                .andExpect(jsonPath("$.balance[1].amount").value(0.0))
+                .andExpect(jsonPath("$.balance[1].currency").isString());
+    }
+
+    @Test
+    void givenInvalidCustomer_whenInsertAccount_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"customerId\":" + 9999 +",\"countryCode\":\"EE\",\"currencyCodes\":[\"USD\",\"EUR\"]}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Customer not found"));
+    }
+
+    @Test
+    void givenExistingAccount_whenInsertAccount_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"customerId\":" + customerId +",\"countryCode\":\"EE\",\"currencyCodes\":[\"USD\",\"EUR\"]}"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"customerId\":" + customerId +",\"countryCode\":\"EE\",\"currencyCodes\":[\"USD\",\"EUR\"]}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Account exists for this customer"));
+    }
+
+    @Test
+    void givenInvalidCountry_whenInsertAccount_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"customerId\":" + customerId +",\"countryCode\":\"XY\",\"currencyCodes\":[\"USD\",\"EUR\"]}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Country is not supported"));
+    }
+
+    @Test
+    void givenInvalidCurrency_whenInsertAccount_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"customerId\":" + customerId +",\"countryCode\":\"BD\",\"currencyCodes\":[\"USD\",\"XYZ\"]}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Invalid or unsupported currency"));
+    }
+
+    @AfterEach
+    void teardown() {
+        if(customerId > 0) {
+            customerMapper.deleteCustomerById(customerId);
+        }
+    }
+}
---
+++ src/test/java/co/modularbank/banking/integration/TransactionTest.java
@@ -0,0 +1,193 @@
+package co.modularbank.banking.integration;
+
+import co.modularbank.banking.amqp.RabbitMessageListener;
+import co.modularbank.banking.controller.model.AccountRequest;
+import co.modularbank.banking.controller.model.TransactionRequest;
+import co.modularbank.banking.domain.Customer;
+import co.modularbank.banking.domain.TransactionDirection;
+import co.modularbank.banking.mapper.CustomerMapper;
+import co.modularbank.banking.service.AccountService;
+import co.modularbank.banking.service.TransactionService;
+import org.hamcrest.Matchers;
+import org.hamcrest.number.IsCloseTo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TransactionTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    CustomerMapper customerMapper;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    TransactionService transactionService;
+
+    @MockBean
+    RabbitMessageListener rabbitMessageListener;
+
+    private long customerId;
+    private long accountId;
+
+    @BeforeEach
+    void setup() {
+        Customer newCustomer = new Customer("Balance", "Mapper", UUID.randomUUID() + "@gmail.com", "11223344");
+        customerId = customerMapper.insertCustomer(newCustomer);
+
+        AccountRequest accountRequest = new AccountRequest();
+        accountRequest.setCustomerId(customerId);
+        accountRequest.setCountryCode("BD");
+        Set<String> currencyList = new HashSet<>();
+        currencyList.add("USD");
+        currencyList.add("EUR");
+
+        accountRequest.setCurrencyCodes(currencyList);
+
+        Assertions.assertDoesNotThrow(() -> {
+            accountId = accountService.createAccount(accountRequest).getAccountId();
+        });
+    }
+
+    private TransactionRequest getTransactionRequest(long tempAccountId) {
+        TransactionRequest request = new TransactionRequest();
+        request.setAccountId(tempAccountId);
+        request.setAmount(2.2);
+        request.setCurrency("USD");
+        request.setDirection(TransactionDirection.IN.name());
+        request.setDescription("Transaction #In -> $2.2");
+        return request;
+    }
+
+    @Test
+    void givenTransactionRequest_whenMakeTransaction_thenCheckSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":2.2,\"currency\":\"EUR\",\"direction\":\"IN\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(IsCloseTo.closeTo(2.2, 0.0001)))
+                .andExpect(jsonPath("$.amount").value(IsCloseTo.closeTo(2.2, 0.0001)))
+                .andExpect(jsonPath("$.currency").value("EUR"));
+    }
+
+    @Test
+    void givenMultipleTransactionRequest_whenMakeTransaction_thenCheckSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":2.2,\"currency\":\"EUR\",\"direction\":\"IN\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(IsCloseTo.closeTo(2.2, 0.0001)));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":2.2,\"currency\":\"EUR\",\"direction\":\"IN\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(IsCloseTo.closeTo(4.4, 0.0001)));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":2.2,\"currency\":\"EUR\",\"direction\":\"IN\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(IsCloseTo.closeTo(6.6, 0.0001)));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":2.2,\"currency\":\"EUR\",\"direction\":\"OUT\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value(IsCloseTo.closeTo(4.4, 0.0001)));
+    }
+
+    @Test
+    void givenInvalidAccountId_whenMakeTransaction_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + 99999 + ",\"amount\":5.0,\"currency\":\"USD\",\"direction\":\"IN\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Account missing"));
+    }
+
+    @Test
+    void givenInvalidCurrencyId_whenMakeTransaction_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":5.0,\"currency\":\"ABC\",\"direction\":\"IN\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Invalid currency"));
+    }
+
+    @Test
+    void givenInActiveCurrencyId_whenMakeTransaction_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":5.0,\"currency\":\"GBP\",\"direction\":\"IN\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Currency not available for this account"));
+    }
+
+    @Test
+    void givenLargeOUTAmountThenBalance_whenMakeTransaction_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"accountId\":" + accountId + ",\"amount\":5.0,\"currency\":\"EUR\",\"direction\":\"OUT\",\"description\":\"Transaction 1\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Insufficient funds"));
+    }
+
+    @Test
+    void givenAccountId_whenRequestAllTransaction_thenCheckCorrect() throws Exception {
+        TransactionRequest request = getTransactionRequest(accountId);
+
+        transactionService.makeTransaction(request);
+        transactionService.makeTransaction(request);
+        transactionService.makeTransaction(request);
+
+        request.setDirection(TransactionDirection.OUT.name());
+        transactionService.makeTransaction(request);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/transaction/account/" + accountId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(4)))
+                .andExpect(jsonPath("$[0].direction").value("OUT"))
+                .andExpect(jsonPath("$[0].amount").value(IsCloseTo.closeTo(2.2, 0.0001)));
+    }
+    @Test
+    void givenInvalidAccountId_whenRequestAllTransaction_thenCheckFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/transaction/account/9999")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Invalid account"));
+    }
+
+    @AfterEach
+    void teardown() {
+        if(customerId > 0) {
+            customerMapper.deleteCustomerById(customerId);
+        }
+    }
+}
